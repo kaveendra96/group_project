f5573af54ee05ead8dd754fc369d64fc6a08d60f
migara96 <47963309+migara96@users.noreply.github.com>
2020-01-07 10:41:11 +0530

    renamed class and file name passwordEncoder instead of passwordEnconder

--- src/main/java/lk/cmb/ucsc/drivingschool/security/PasswordEncoderTest.java
+++ src/main/java/lk/cmb/ucsc/drivingschool/security/PasswordEncoderTest.java
@@ -2,7 +2,7 @@ package lk.cmb.ucsc.drivingschool.security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class PasswordEnconderTest implements PasswordEncoder {
+public class PasswordEncoderTest implements PasswordEncoder {
     @Override
     public String encode(CharSequence charSequence) {
         return charSequence.toString();
