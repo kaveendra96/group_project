edbc3bb1078a238b46fc4b8f39a76dd3c00a74a4
Sakuni <saku9706@gmail.com>
2020-01-20 15:58:58 +0530

    model file for Quiz

---
+++ src/main/java/lk/cmb/ucsc/drivingschool/controller/QuizController.java
@@ -0,0 +1,19 @@
+/*package lk.cmb.ucsc.drivingschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class QuizController {
+    @GetMapping("index")
+    public String index(){
+        return "student/index";
+    }
+
+
+
+
+}
+*/
