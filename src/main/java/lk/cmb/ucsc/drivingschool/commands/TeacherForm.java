14cc7d4d0eb6165b4e200230d2f03270777adc21
MadurangiChathurika <43290660+MadurangiChathurika@users.noreply.github.com>
2020-02-13 11:44:34 +0530

    comands
    add  commands directory with TeacherForm.java file

---
+++ src/main/java/lk/cmb/ucsc/drivingschool/commands/TeacherForm.java
@@ -0,0 +1,49 @@
+package lk.cmb.ucsc.drivingschool.commands;
+
+public class TeacherForm {
+    private String id;
+    private String name;
+    private String competence;
+    private String attendence;
+    private String comment;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompetence() {
+        return competence;
+    }
+
+    public void setCompetence(String competence) {
+        this.competence = competence;
+    }
+
+    public String getAttendence() {
+        return attendence;
+    }
+
+    public void setAttendence(String attendence) {
+        this.attendence = attendence;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
