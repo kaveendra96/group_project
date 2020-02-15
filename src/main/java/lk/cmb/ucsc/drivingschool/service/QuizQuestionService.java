d4e1f45b69f550226f838dddac8444ac0971c392
Sakuni <saku9706@gmail.com>
2020-02-15 21:34:34 +0530

    edit quizquestion service

--- src/main/java/lk/cmb/ucsc/drivingschool/service/QuizQuestionService.java
+++ src/main/java/lk/cmb/ucsc/drivingschool/service/QuizQuestionService.java
@@ -5,6 +5,9 @@ import lk.cmb.ucsc.drivingschool.repo.QuizQuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QuizQuestionService {
     @Autowired
@@ -18,12 +21,23 @@ public class QuizQuestionService {
         return quizquestionRepo.findById(id);
     }
 
+    public List<QuizQuestion> listAll() {
+        List<QuizQuestion> quizquestions = new ArrayList<>();
+        quizquestionRepo.findAll().forEach(quizquestions::add);
+        return quizquestions;
+    }
+
     //delete question
     public void delete(String id){
         QuizQuestion quizquestion=quizquestionRepo.findById(id);
         quizquestionRepo.delete(quizquestion);
     }
 
+    public QuizQuestion saveOrUpdate(QuizQuestion quizquestion) {
+        quizquestionRepo.save(quizquestion);
+        return quizquestion;
+    }
+    /*
     //update question
     public QuizQuestion updateQuestion(String id, String question){
         QuizQuestion quizquestion=quizquestionRepo.findById(id);
@@ -31,11 +45,31 @@ public class QuizQuestionService {
         return quizquestionRepo.save(quizquestion);
     }
 
-    public QuizQuestion updateAnswer(String id, String correct){
+    public QuizQuestion updateCorrect(String id, String correct){
         QuizQuestion quizquestion=quizquestionRepo.findById(id);
         quizquestion.setCorrect(correct);
         return quizquestionRepo.save(quizquestion);
     }
 
 
+    public QuizQuestion updateIncorrect1(String id, String incorrect1){
+        QuizQuestion quizquestion=quizquestionRepo.findById(id);
+        quizquestion.setIncorrect1(incorrect1);
+        return quizquestionRepo.save(quizquestion);
+    }
+
+    public QuizQuestion updateIncorrect2(String id, String incorrect2){
+        QuizQuestion quizquestion=quizquestionRepo.findById(id);
+        quizquestion.setIncorrect2(incorrect2);
+        return quizquestionRepo.save(quizquestion);
+    }
+
+    public QuizQuestion updateIncorrect3(String id, String incorrect3){
+        QuizQuestion quizquestion=quizquestionRepo.findById(id);
+        quizquestion.setIncorrect3(incorrect3);
+        return quizquestionRepo.save(quizquestion);
+    }
+
+
+*/
 }
